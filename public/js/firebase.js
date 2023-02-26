97d906b91eba7808487cd069d82dc23ce719edb7
Zdravko420 <43958501+Zdravko420@users.noreply.github.com>
2023-02-26 20:10:10 +0200

    main code added

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyC4Xweb_K9vWF6oxIO8NeUyVVQKCXZogIw",
+    authDomain: "note-app-73368.firebaseapp.com",
+    projectId: "note-app-73368",
+    storageBucket: "note-app-73368.appspot.com",
+    messagingSenderId: "358165143119",
+    appId: "1:358165143119:web:17240b42628ed2ccd0d43a"
+  };
+
+  // Initialize Firebase
+  let app = initializeApp(firebaseConfig);
+  let db = firebase.firestore();
