ee739ab1be80aef25481effd33a27f6ebd449eba
Zdravko420 <43958501+Zdravko420@users.noreply.github.com>
2023-02-28 08:33:41 +0200

    Added login and registration

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,7 +1,11 @@
-const blogTitleField = document.querySelector('.title');
-const articleFeild = document.querySelector('.article');
+import {firestore} from "./firebase.js";
+//import {getFirestore, collection, getDocs} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
+import {setDoc, doc, getDoc, addDoc, collection} from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js';
+//import { } from 'https://www.gstatic.com/firebasejs/ui/9.8.1/firebase-ui-auth.js';
 
-// banner
+const noteTitleField = document.querySelector('.title');
+const articleField = document.querySelector('.article');
+
 const bannerImage = document.querySelector('#banner-upload');
 const banner = document.querySelector(".banner");
 let bannerPath;
@@ -9,72 +13,71 @@ let bannerPath;
 const publishBtn = document.querySelector('.publish-btn');
 const uploadInput = document.querySelector('#image-upload');
 
+
 bannerImage.addEventListener('change', () => {
     uploadImage(bannerImage, "banner");
 })
 
-uploadInput.addEventListener('change', () => {
+uploadInput.addEventListener('change', () =>{
     uploadImage(uploadInput, "image");
 })
 
-const uploadImage = (uploadFile, uploadType) => {
+const uploadImage = (uploadFile, uploadType) => { 
     const [file] = uploadFile.files;
-    if(file && file.type.includes("image")){
+    if(file && file.type.includes("image")){ 
         const formdata = new FormData();
         formdata.append('image', file);
 
-        fetch('/upload', {
-            method: 'post',
+        fetch('/upload',{
+            method: 'post', 
             body: formdata
         }).then(res => res.json())
         .then(data => {
-            if(uploadType == "image"){
+            if(uploadType == "image"){ 
                 addImage(data, file.name);
-            } else{
+            }else{ 
                 bannerPath = `${location.origin}/${data}`;
-                banner.style.backgroundImage = `url("${bannerPath}")`;
+            banner.style.backgroundImage = `url("${bannerPath}")`;
             }
         })
-    } else{
+    }else{ 
         alert("upload Image only");
     }
 }
 
 const addImage = (imagepath, alt) => {
-    let curPos = articleFeild.selectionStart;
+    let curPos = articleField.selectionStart;
     let textToInsert = `\r![${alt}](${imagepath})\r`;
-    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
+    articleField.value = articleField.value.slice(0, curPos) + textToInsert + articleField.value.slice(curPos);
 }
 
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-publishBtn.addEventListener('click', () => {
-    if(articleFeild.value.length && blogTitleField.value.length){
-        // generating id
+publishBtn.addEventListener('click', async () => {
+    if(articleField.value.length && noteTitleField.value.length){
+        //generirane na id
         let letters = 'abcdefghijklmnopqrstuvwxyz';
-        let blogTitle = blogTitleField.value.split(" ").join("-");
+        let noteTitle = noteTitleField.value.split(" ").join("-");
         let id = '';
-        for(let i = 0; i < 4; i++){
+        for(let i = 0; i <4; i++){
             id += letters[Math.floor(Math.random() * letters.length)];
         }
 
-        // setting up docName
-        let docName = `${blogTitle}-${id}`;
-        let date = new Date(); // for published at info
+        //suzdawane imena za doc
+        let docName = `${noteTitle}-${id}`;
+        let date = new Date();
 
-        //access firstore with db variable;
-        
-        db.collection("blogs").doc(docName).set({
-            title: blogTitleField.value,
-            article: articleFeild.value,
+        //dostup do firestore s db promenliwa
+        //let notesData = doc(firestore, `notes`);
+        await addDoc(collection(firestore, 'notes'),{
+            title: noteTitleField.value,
+            article: articleField.value,
             bannerImage: bannerPath,
             publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
         })
-        .then(() => {
-            location.href = `/${docName}`;
+        .then(() =>{
+            location.href = `/note.html`;
         })
-        .catch((err) => {
-            console.error(err);
-        })
+
     }
 })
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express =  require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
 
@@ -9,41 +9,38 @@ app.use(express.static(initial_path));
 app.use(fileupload());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(initial_path, "home.html"));
+    res.sendFile(path.join(initial_path, "home.html"))
 })
 
 app.get('/editor', (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"));
 })
 
-// upload link
 app.post('/upload', (req, res) => {
     let file = req.files.image;
     let date = new Date();
-    // image name
+
     let imagename = date.getDate() + date.getTime() + file.name;
-    // image upload path
+
     let path = 'public/uploads/' + imagename;
 
-    // create upload
     file.mv(path, (err, result) => {
         if(err){
             throw err;
         } else{
-            // our image upload path
             res.json(`uploads/${imagename}`)
         }
     })
 })
-
-app.get("/:blog", (req, res) => {
-    res.sendFile(path.join(initial_path, "blog.html"));
+app.get("/admin", (req, res) => {
+    res.sendFile(path.join(initial_path, "dashboard.html"));
 })
-
+app.get("/note", (req, res) => {
+    res.sendFile(path.join(initial_path, "note.html"));
+})
 app.use((req, res) => {
     res.json("404");
 })
-
 app.listen("3000", () => {
     console.log('listening......');
 })
