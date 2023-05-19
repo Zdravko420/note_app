29e97bffa7357b33d6203f3c6957399afd6a0e91
Zdravko420 <43958501+Zdravko420@users.noreply.github.com>
2023-05-19 18:42:34 +0300

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -11,11 +11,11 @@ import { getFirestore, doc, setDoc, collection, getDoc } from "https://www.gstat
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4Xweb_K9vWF6oxIO8NeUyVVQKCXZogIw",
-    authDomain: "note-app-73368.firebaseapp.com",
-    projectId: "note-app-73368",
-    storageBucket: "note-app-73368.appspot.com",
-    messagingSenderId: "358165143119",
-    appId: "1:358165143119:web:17240b42628ed2ccd0d43a"
+  authDomain: "note-app-73368.firebaseapp.com",
+  projectId: "note-app-73368",
+  storageBucket: "note-app-73368.appspot.com",
+  messagingSenderId: "358165143119",
+  appId: "1:358165143119:web:17240b42628ed2ccd0d43a"
 };
 
 // Initialize Firebase
--- server.js
+++ server.js
@@ -43,4 +43,5 @@ app.use((req, res) => {
 })
 app.listen("3000", () => {
     console.log('listening......');
+    
 })
